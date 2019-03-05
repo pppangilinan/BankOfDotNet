45dbe8a34b81c6cdd59f9c1cd90a80e90cd60ebe
Patrick Pangilinan <patrick@hirum.com.au>
2019-03-05 16:26:17 +0800

    2. .NET Core Web API Project Setup

---
+++ BankOfDotNet/BankOfDotNet.Api/Models/BankContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankOfDotNet.Api.Models
+{
+    public class BankContext : DbContext
+    {
+        public BankContext(DbContextOptions<BankContext> options)
+            : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
